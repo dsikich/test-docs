0b16d84fd265fb0e39a209dec581dd1a85a378d6
Danielle Sikich <sikich1@llnl.gov>
2018-06-27 10:42:00 -0700

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import guzzle_sphinx_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
